670d79d3200cfed8e79443dd3a3d68d8428d962c
Steffen Kowalski <scipper@myscipper.de>
2024-08-09 21:32:36 +0200

    enabled source maps

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ const config = {
     output: {
         path: path.resolve(__dirname, "dist")
     },
+    devtool: "source-map",
     devServer: {
         open: true,
         host: "localhost",
