d67faa36489d125f94f2aa99bdd63923b4a59127
Steffen Kowalski <scipper@myscipper.de>
2024-11-21 20:30:25 +0100

    added docker compose

--- src/acceptance/test.ts
+++ src/acceptance/test.ts
@@ -2,16 +2,35 @@ import {Builder, Browser, By} from "selenium-webdriver";
 import {assert} from "chai";
 import {Options} from "selenium-webdriver/chrome";
 
+let maxRetries = 3;
+const timeBetweenRetriesInSeconds = 3 * 1000;
+
 async function run() {
+  const seleniumServer = "http://chrome-webdriver:4444/wd/hub";
+
+  do {
+    maxRetries--;
+    try {
+      await fetch(seleniumServer);
+    } catch(_) {
+      await new Promise((resolve) => setTimeout(resolve, timeBetweenRetriesInSeconds));
+    }
+  } while(maxRetries > 0);
+
+  if(maxRetries === 0) {
+    throw new Error("Selenium Server not reachable");
+  }
+
+
   const driverBuilder = new Builder()
     .forBrowser(Browser.CHROME);
   const options = new Options();
   options.addArguments("--headless");
-  options.addArguments("--no-sandbox");
 
+  options.addArguments("--no-sandbox");
   const driver = await driverBuilder
     .setChromeOptions(options)
-    .usingServer("http://chrome-webdriver:4444/wd/hub")
+    .usingServer(seleniumServer)
     .build();
   try {
     await driver.get("http://gui:8080");
