e92bd6dd970575c9ddb770d463fa1efb6c356b69
Steffen Kowalski <scipper@myscipper.de>
2025-02-12 20:38:48 +0100

    made all working

--- src/acceptance/test.ts
+++ src/acceptance/test.ts
@@ -2,26 +2,9 @@ import {Builder, Browser, By} from "selenium-webdriver";
 import {assert} from "chai";
 import {Options} from "selenium-webdriver/chrome";
 
-let maxRetries = 3;
-const timeBetweenRetriesInSeconds = 3 * 1000;
-
 async function run() {
   const seleniumServer = "http://chrome-webdriver:4444/wd/hub";
   const gui = "http://gui:80";
-
-  // do {
-  //   maxRetries--;
-  //   try {
-  //     await fetch(seleniumServer);
-  //   } catch(_) {
-  //     await new Promise((resolve) => setTimeout(resolve, timeBetweenRetriesInSeconds));
-  //   }
-  // } while(maxRetries > 0);
-  //
-  // if(maxRetries === 0) {
-  //   throw new Error("Selenium Server not reachable");
-  // }
-
 
   const driverBuilder = new Builder()
     .forBrowser(Browser.CHROME);
--- src/main/ts/Main.ts
+++ src/main/ts/Main.ts
@@ -4,11 +4,10 @@ import "../resources/styles/style.scss";
 @MainClass
 export class Main {
 
-  public static main(): void {
-    const h1 = document.querySelector("h1");
-    if(h1) {
-      h1.innerText = "It's working!";
+    public static main(): void {
+        const h1 = document.createElement("h1");
+        h1.innerText = "It's working!";
+        document.body.insertAdjacentElement("beforeend", h1);
     }
-  }
 
 }
